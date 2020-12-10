d82a3488bf7431bf753a9df82e5f169771d6b85a
aido123 <adrianhynes@gmail.com>
2020-12-10 22:22:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ansible-galaxy collection install community.kubernetes
 git clone https://github.com/ansible-collections/azure.git
 cp ansible/azure_rm_resource_info_rt.py azure/plugins/modules/azure_rm_resource_info_rt.py
 pip install -r azure/requirements-azure.txt
+cd azure
 ansible-galaxy collection build --force
 ansible-galaxy collection install azure-azcollection-1.2.0.tar.gz --force
